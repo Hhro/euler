6cf2bc64e85c23963fdb54e6fba54b565a4fd719
Hhro <seungwan.kwon@prosys.kr>
2023-07-10 00:36:36 +0900

    [euler] 16

--- bin/main.ml
+++ bin/main.ml
@@ -341,9 +341,30 @@ let chall_14 () =
 
 let chall_15 () = Math.choose 40 20
 
+let chall_16 () =
+  let solve () =
+    let base = Big_int_Z.big_int_of_int 2 in
+    let divisor = Big_int_Z.big_int_of_int 10 in
+    let zero = Big_int_Z.zero_big_int in
+    let rec mul acc i =
+      if i = 1000 then acc else mul (Big_int_Z.mult_big_int base acc) (i + 1)
+    in
+    let rec sum acc n =
+      if Big_int_Z.eq_big_int n zero
+      then acc
+      else
+        sum
+          (Big_int_Z.add_big_int acc (Big_int_Z.mod_big_int n divisor))
+          (Big_int_Z.div_big_int n divisor)
+    in
+    sum zero (mul base 1) |> Big_int_Z.int_of_big_int
+  in
+  solve ()
+;;
+
 let () =
   let t = Sys.time () in
-  let answer = chall_15 () in
+  let answer = chall_16 () in
   Format.printf "elapsed: %f" (Sys.time () -. t);
   answer |> string_of_int |> print_endline
 ;;
