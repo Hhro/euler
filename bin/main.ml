638837b3435ead18b14a21a4a830102f53f31af9
Hhro <seungwan.kwon@prosys.kr>
2023-07-10 02:22:51 +0900

    [euler] 20

--- bin/main.ml
+++ bin/main.ml
@@ -401,6 +401,28 @@ let chall_18 () =
   solve ()
 ;;
 
+let chall_20 () =
+  let solve () =
+    let divisor = Big_int_Z.big_int_of_int 10 in
+    let zero = Big_int_Z.zero_big_int in
+    let rec fact n =
+      if n = 0
+      then Big_int_Z.big_int_of_int 1
+      else Big_int_Z.mult_big_int (Big_int_Z.big_int_of_int n) (fact (n - 1))
+    in
+    let rec sum acc n =
+      if Big_int_Z.eq_big_int n zero
+      then acc
+      else
+        sum
+          (Big_int_Z.add_big_int acc (Big_int_Z.mod_big_int n divisor))
+          (Big_int_Z.div_big_int n divisor)
+    in
+    sum zero (fact 100) |> Big_int_Z.int_of_big_int
+  in
+  solve ()
+;;
+
 let chall_67 () =
   let in_channel = open_in "data/67.txt" in
   let rec input_nums nums i =
@@ -435,7 +457,7 @@ let chall_67 () =
 
 let () =
   let t = Sys.time () in
-  let answer = chall_18 () in
+  let answer = chall_20 () in
   Format.printf "elapsed: %f" (Sys.time () -. t);
   answer |> string_of_int |> print_endline
 ;;
